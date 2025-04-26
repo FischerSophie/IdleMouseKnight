0921355b156309c44809f301f7391c8dcc4f5f8e
TheSpoonProblem <Sophie.elisabeth.fsicher@gmail.com>
2025-04-26 17:40:45 +0200

    Projekt Base

--- idlemouseknight/Scripts/VisitCastle.gd
+++ idlemouseknight/Scripts/VisitCastle.gd
@@ -1,13 +1,9 @@
 extends Node
 
-"Object DUNGON = Dungon"
+"""Object DUNGON = Dungon"
 "CharacterBody2D Player = Chacter_Base" 
 "List EQIPPMENt= EqimeTable"
 " List WOunds = WoundTable"
-
-
-
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,13 +11,14 @@ func _ready() -> void:
 	pass # Replace with function body.
 	"LoadRandom Dungon"
 	
-"func_playerBesuchtDungon"
+" func_playerBesuchtDungon"
    "ersetze playersysmbol zu Dungonsymbol"
    "Vergeliche Dungon Werte mit Player werten"
    " Wenn Playerwert gößer als Dungonwert = sucsess"
    "Bei sucsess = random Equipment"
    " Call Function Attach Equipment to player"
-   " bei Niederlage = schaden + Schadenseffect "
+   " bei Niederlage = schaden + Schadenseffect
+   load new dungon"""
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
